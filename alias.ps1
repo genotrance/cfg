95d8f522bb046598bf92acfaf005f0b376ab2616
Ganesh Viswanathan <dev@genotrance.com>
2020-07-14 22:23:39 -0500

    Windows changes

--- alias.ps1
+++ alias.ps1
@@ -6,10 +6,10 @@ doskey /exename=powershell.exe l=busybox ls $*
 doskey /exename=powershell.exe ll=busybox ls -al $*
 doskey /exename=powershell.exe ng=nimgrep --recursive --ext:$*
 doskey /exename=powershell.exe nimgdb=gdb -x $env:USERPROFILE\Desktop\DL\programming\nimdevel\tools\nim-gdb.py $*
-doskey /exename=powershell.exe rm=busybox rm $*
 doskey /exename=powershell.exe x=exit
 
 doskey /exename=powershell.exe hcd=cd $env:USERPROFILE\$*
 doskey /exename=powershell.exe pcd=cd $env:USERPROFILE\Desktop\DL\programming\$*
+doskey /exename=powershell.exe ncd=cd $env:USERPROFILE\nimcache\nimterop\$*
 doskey /exename=powershell.exe gclone=git clone https://github.com/$*
 doskey /exename=powershell.exe tkns=taskkill /f /im nimsuggest.exe
